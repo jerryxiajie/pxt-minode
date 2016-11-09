bffcb2736ceac1a078994c93accda218b4af6b99
jerryxiajie <jerry.xia@embest-tech.com>
2016-11-09 14:00:51 +0800

    move code from extension.cpp to pxt_minode.cpp

--- extension.ts
+++ extension.ts
@@ -20,7 +20,7 @@ namespace minode {
 
   //% blockId=device_switch_is_opened block="switch %connName| is opened"
   //% shim=minode::switchIsOened
-  function switchIsOened(connName : ConnName): boolean
+  export function switchIsOened(connName : ConnName): boolean
   {
     return;
   }
@@ -30,7 +30,7 @@ namespace minode {
  */
   //% blockId=device_fan_control block="fan %connName| is %FanStatus"
   //% shim=minode::FanControl
-  function FanControl(connName : ConnName , status : FanStatus): void
+  export function FanControl(connName : ConnName , status : FanStatus): void
   {
   }
 
@@ -39,7 +39,7 @@ namespace minode {
   */
   //% blockId=device_DHT_GET_Temperature block="DHT11 %connName| tempreature %FanStatus"
   //% shim=minode::DHTGetTemperature
-  function DHTGetTemperature(connName : ConnName,style : DHTTemStyle) : number
+  export function DHTGetTemperature(connName : ConnName,style : DHTTemStyle) : number
   {
     return;
   }
@@ -49,7 +49,7 @@ namespace minode {
   */
   //% blockId=device_DHT_GET_Humidity block="DHT11 %connName| humidity"
   //% shim=minode::DHTGetHumidity
-  function DHTGetHumidity(connName : ConnName) : number
+  export function DHTGetHumidity(connName : ConnName) : number
   {
     return;
   }
@@ -59,7 +59,7 @@ namespace minode {
   */
   //% blockId=device_on_DHTtemperature_CHANGE block="on DHT11 %connName| temperature change."
   //% shim=minode::onDHTEvent
-  function onDHTEvent(connName : ConnName,body : Action) : void
+  export function onDHTEvent(connName : ConnName,body : Action) : void
   {
   }
 
@@ -68,7 +68,7 @@ namespace minode {
   */
   //% blockId=device_on_PIR_trig block="PIR %connName| on trigger"
   //% shim=minode::onPIREvent
-  function onPIREvent(connName : ConnName, body : Action) : void
+  export function onPIREvent(connName : ConnName, body : Action) : void
   {
   }
 
@@ -77,7 +77,7 @@ namespace minode {
   */
   //% blockId=device_PIR_istrig block="PIR %connName| is triggered"
   //% shim=minode::PIRIsTriggered
-  function PIRIsTriggered(connName : ConnName) : boolean
+  export function PIRIsTriggered(connName : ConnName) : boolean
   {
     return;
   }
@@ -87,7 +87,7 @@ namespace minode {
    */
   //% blockId=device_on_ROTARY_CHANGE block="Rotary %connName| on trigger"
   //% shim=minode::onRotaryEvent
-  function onRotaryEvent(connName : ConnName, body : Action) : void
+  export function onRotaryEvent(connName : ConnName, body : Action) : void
   {
   }
 
@@ -96,7 +96,7 @@ namespace minode {
   */
   //% blockId=device_ROTARY_GET_Percentage block="ROTARY get %connName| Percentage"
   //% shim=minode::RotaryGetPercentage
-  function RotaryGetPercentage(connName : ConnName) : number
+  export function RotaryGetPercentage(connName : ConnName) : number
   {
     return;
   }
@@ -107,7 +107,7 @@ namespace minode {
   //% blockId=device_on_LightSensor_CHANGE block="LightSensor %connName| on change"
   //% advanced=true
   //% shim=minode::onLightSensorEvent
-  function onLightSensorEvent(connName : ConnName, body : Action) : void
+  export function onLightSensorEvent(connName : ConnName, body : Action) : void
   {
   }
 
@@ -117,7 +117,7 @@ namespace minode {
   //% blockId=device_LightSensor_GET_light_level block="LightSensor get %connName| level"
   //% advanced=true
   //% shim=minode::LightSensorGetLevel
-  function LightSensorGetLevel(connName : ConnName) : number
+  export function LightSensorGetLevel(connName : ConnName) : number
   {
     return;
   }
@@ -128,7 +128,7 @@ namespace minode {
   //% blockId=device_on_MIC_level_change block="MIC %connName| on change"
   //% advanced=true
   //% shim=minode::onMICEvent
-  function onMICEvent(connName : ConnName,body : Action) : void
+  export function onMICEvent(connName : ConnName,body : Action) : void
   {
   }
 
@@ -138,7 +138,7 @@ namespace minode {
   //% blockId=device_MIC_GET_mic_level block="MIC get %connName| level"
   //% advanced=true
   //% shim=minode::MICGetLevel
-  function MICGetLevel(connName : ConnName) : number
+  export function MICGetLevel(connName : ConnName) : number
   {
     return;
   }
@@ -149,7 +149,7 @@ namespace minode {
   //% blockId=device_RGB_ChooseColor block="RGB %connName| set %MiNodeColor"
   //% advanced=true
   //% shim=minode::RGBChooseColor
-  function RGBChooseColor(connName : ConnName ,color : MiNodeColor) : void
+  export function RGBChooseColor(connName : ConnName ,color : MiNodeColor) : void
   {
   }
 
@@ -159,7 +159,7 @@ namespace minode {
   //% blockId=device_RGB_SetColor block="RGB %connName| set %text"
   //% advanced=true
   //% shim=minode::RGBSetColor
-  function RGBSetColor(connName : ConnName,text : number) : void
+  export function RGBSetColor(connName : ConnName,text : number) : void
   {
   }
 
